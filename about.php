89180468c79ef453664b341f57c60b551bf30c8c
Rash <113658888+Rashfox@users.noreply.github.com>
2025-07-04 21:49:44 +0700

    update footer

--- about.php
+++ about.php
@@ -32,7 +32,7 @@
     <section>
     </section>
     <footer>
-        <p>&copy; 2025 ETournament. All Right Reserved.</p>
+        <p>&copy; 2025 ETournament | Kelompok 3. All Right Reserved.</p>
     </footer>
 </body>
 </html>
